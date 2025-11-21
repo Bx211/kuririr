bfe8e5979a870fe05839b236d31b78b42e8e4a37
Bx211 <h123.bocah3@gmail.com>
2025-11-22 02:26:23 +0700

    Add initial test for CleanCommand class

--- test/java/com/kuririr/cli/CleanCommandTest.java
+++ test/java/com/kuririr/cli/CleanCommandTest.java
@@ -1,1 +1,13 @@
+package com.kuririr.cli;
 
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CleanCommandTest {
+@Test
+void testClean() {
+CleanCommand cmd = new CleanCommand();
+}
+}
