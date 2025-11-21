5cb7f0ed1a4655fc0db517ef656a2cf010450022
Bx211 <h123.bocah3@gmail.com>
2025-11-22 02:24:46 +0700

    Update Main.java

--- src/main/java/com/kuririr/cli/Main.java
+++ src/main/java/com/kuririr/cli/Main.java
@@ -1,1 +1,21 @@
+package com.kuririr.cli;
 
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+
+@Command(
+name = "kuririr",
+subcommands = {CleanCommand.class, SlugCommand.class, CountCommand.class}
+)
+public class Main implements Runnable {
+public static void main(String[] args) {
+new CommandLine(new Main()).execute(args);
+}
+
+
+public void run() {
+System.out.println("Gunakan subcommand: clean, slug, count");
+}
+}
