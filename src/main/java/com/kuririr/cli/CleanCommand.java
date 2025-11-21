e772829cc1110545fca6cc1f2157a169cf382e62
Bx211 <h123.bocah3@gmail.com>
2025-11-22 02:25:08 +0700

    Update CleanCommand.java

--- src/main/java/com/kuririr/cli/CleanCommand.java
+++ src/main/java/com/kuririr/cli/CleanCommand.java
@@ -1,1 +1,19 @@
+package com.kuririr.cli;
 
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+
+@Command(name = "clean", description = "Hapus simbol dari teks")
+public class CleanCommand implements Runnable {
+
+
+@Parameters(paramLabel = "TEXT")
+private String text;
+
+
+public void run() {
+System.out.println(text.replaceAll("[^a-zA-Z0-9 ]+", ""));
+}
+}
