26f6cee8268decaff30d722636073295075ab478
Bx211 <h123.bocah3@gmail.com>
2025-11-22 02:26:40 +0700

    Update SlugCommandTest.java

--- test/java/com/kuririr/cli/SlugCommandTest.java
+++ test/java/com/kuririr/cli/SlugCommandTest.java
@@ -1,1 +1,13 @@
+package com.kuririr.cli;
 
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SlugCommandTest {
+@Test
+void testSlug() {
+SlugCommand cmd = new SlugCommand();
+}
+}
